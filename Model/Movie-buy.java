a8ac86dd48b92d2a9871da891364e6491f9f14b7
Arletaxx <arletawasiuk@wp.pl>
2014-12-03 13:09:25 +0100

    Laboratorium 9 - projekt sklepu

---
+++ Model/Movie-buy.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Movie-buy extends Movie {
+
+    /**
+     * 
+     */
+    public Movie-buy() {
+    }
+
+    /**
+     * 
+     */
+    public void Stato: {presente, da ordinare};
+
+    /**
+     * 
+     */
+    public void prezzo-vendita;
+
+
+}
